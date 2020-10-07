244149957d3b68c41a966794664820b02b42a5d9
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2020-10-07 10:02:36 -0300

    insulating components

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Content, Background } from './styles';
 
-const Signin: React.FC = () => (
+const SignIn: React.FC = () => (
   <Container>
     <Content>
       <img src={logoImg} alt="GoBarber" />
@@ -13,11 +16,16 @@ const Signin: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <input type="passaword" placeholder="Senha" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="passaword"
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
@@ -31,4 +39,4 @@ const Signin: React.FC = () => (
   </Container>
 );
 
-export default Signin;
+export default SignIn;
