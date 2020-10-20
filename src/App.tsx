f0ac555bf6e3bbc32190aea2021da763ca4ab5cc
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2020-10-19 23:20:47 -0300

    Creating toast hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,13 @@ import Signin from './pages/Signin';
 // import Signup from './pages/Signup';
 import GlobalStyle from './styles/global';
 
-import ToastContainer from './toastContainer/index';
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <Signin />
-    </AuthProvider>
-
-    <ToastContainer />
+    </AppProvider>
 
     <GlobalStyle />
   </>
