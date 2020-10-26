4716ed42f747e3cf25f9efb34b2ec01c138b0113
Fernando Hnerique Tozetto <fhtozetto@gmail.com>
2020-10-25 23:06:37 -0300

    Application routes

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -3,7 +3,7 @@ import { FiMail, FiLogIn, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -27,6 +27,8 @@ const SignIn: React.FC = () => {
   const { signIn } = useAuth();
   const { addToast } = useToast();
 
+  const history = useHistory();
+
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
       try {
@@ -47,6 +49,8 @@ const SignIn: React.FC = () => {
           email: data.email,
           password: data.password,
         });
+
+        history.push('/dashboard');
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
@@ -59,11 +63,11 @@ const SignIn: React.FC = () => {
         addToast({
           type: 'error',
           title: 'Erro na Autenticação',
-          description: 'Ocorreu um erro aou fazer login, cheque as credenciais',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais',
         });
       }
     },
-    [signIn, addToast],
+    [signIn, addToast, history],
   );
 
   return (
